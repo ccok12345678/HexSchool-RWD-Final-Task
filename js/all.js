2169b6825d4a3763f39697a82af0f412df9ac49b
Karl Yang <ccok12345678@gmail.com>
2021-09-09 15:53:32 +0800

    RWD updated

--- js/all.js
+++ js/all.js
@@ -3,7 +3,17 @@ $(document).ready(function () {
   // 漢堡選單
   $('.open-menu').click(function (e) { 
     e.preventDefault();
-    $('.container').toggleClass('show');
+    $('body').toggleClass('show');
+  });
+
+  // off canvas 側邊選單 
+  $('.open').click(function (e) { 
+    e.preventDefault();
+    $('body').addClass('open-aside');
+  });
+  $('.close, .aside-bg').click(function (e) { 
+    e.preventDefault();
+    $('body').removeClass('open-aside');
   });
 
 });
